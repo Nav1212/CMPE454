01efc45064c1fd7249ddb9c6dfecdf67b2b56f9c
ben honda <bhonda89@gmail.com>
2023-04-12 15:52:26 -0400

    soft shadows maybe half working

--- a4/src/scene.cpp
+++ a4/src/scene.cpp
@@ -296,22 +296,79 @@ vec3 Scene::raytrace(vec3 &rayStart, vec3 &rayDir, int depth, int thisObjIndex, 
 
                 // Soft shadows
 
-                // Rejection sampling:
-                // choose A, B in [0, 1]
-                float A = 1;
-                float B = 1;
-                while (A + B > 1) {
-                    A = randIn01();
-                    B = randIn01();
+                vec3 tempIout;
+                int raysThatHit = 0;
+
+                for (int i = 0; i < numRaySamples; i++) {
+                    // Rejection sampling:
+                    // choose A, B in [0, 1]
+                    float alpha = 1;
+                    float beta = 1;
+                    while (alpha + beta > 1) {
+                        alpha = randIn01();
+                        beta = randIn01();
+                    }
+                    float gamma = 1 - alpha - beta;
+
+                    // point on tri is P = gamma*v0 + alpha*v1 + beta*v2
+                    vec3 gammaV0 = gamma * tri->verts[0].position;
+                    vec3 alphaV1 = alpha * tri->verts[1].position;
+                    vec3 betaV2 = beta * tri->verts[2].position;
+                    vec3 tempP1 = gammaV0 + alphaV1 + betaV2;
+                    // vec3 tempP1 = (gammaV0 + alphaV1 + betaV2).normalize();
+                    vec3 tempP = tempP1 - P;
+                    float tempPDist = tempP.length();
+                    // tempP = tempP.normalize();
+                    tempP = (1.0 / tempPDist) * tempP;
+
+                    // vec3 intP, intN, intTexCoords;
+                    // float intT;
+                    // int intObjIndex, intObjPartIndex;
+                    // Material *intMat;
+                    // bool found = findFirstObjectInt(P, tempP, objIndex, objPartIndex, intP, intN, intTexCoords, intT,
+                    //                                 intObjIndex, intObjPartIndex, intMat, i);
+
+                    // is dist to point on tri == to actual int point + epsilon?
+                    float epsilon = 0.001;
+
+                    // Next, test t (distance) to see if the ray is blocked
+                    vec3 intP, intN, intTexCoords;
+                    float intT;
+                    int intObjIndex, intObjPartIndex;
+                    Material *intMat;
+
+                    // Is there an object between P and the light?
+                    //
+                    // Note that 'intObjIndex' will return with the index of the
+                    // object that is hit.  So the hit object is objects[intObjIndex].
+
+                    bool found = findFirstObjectInt(P, tempP, objIndex, objPartIndex, intP, intN, intTexCoords, intT,
+                                                    intObjIndex, intObjPartIndex, intMat, i);
+                    // float tempDist = tempP.length();
+
+                    // cout << tempPDist << " ok: " << intT << endl;
+
+                    // if (found && tempPDist < intT + epsilon && tempPDist > intT - epsilon) {
+                    if (found && tempPDist == intT) {
+                        // for a ray that hits...
+                        raysThatHit += 1;
+
+                        // cout << tempPDist << " ok: " << intT << endl;
+                        // cout << (tempPDist == intT) << endl;
+                        vec3 Lr = (2 * (tempP * N)) * N - tempP;
+                        tempIout = tempIout + calcIout(N, tempP, E, Lr, mat->kd, mat->ks, mat->n, tri->mat->Ie);
+                    }
+
+                    // Triangle *currentTri = (Triangle *)objects[i];
                 }
 
-                // point on triangle is P = (1-A-B)v0 + Av1 + Bv2
+                vec3 avgRay = (1.0f / raysThatHit) * tempIout;
+                vec3 intensityOfAvgRay = (raysThatHit / numRaySamples) * avgRay;
 
-                // Next, test t (distance) to see if the ray is blocked
+                // Iout = Iout + (1.0f / numRaySamples) * (avgRay);
+                // Iout = Iout + intensityOfAvgRay;
+                Iout = Iout + (raysThatHit / numRaySamples) * tri->mat->Ie;
 
-                Triangle *currentTri = (Triangle *)objects[i];
-
-                // Iout=Iout+
                 // ---------------- END YOUR CODE HERE ----------------
             }
         }
